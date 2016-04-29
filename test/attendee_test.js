86ad2144058a83d4108b019bd305923fb6add7c0
welcloud <o.lemaitre@welcloud.io>
2016-04-29 13:41:52 +0000

    fix spelling error

--- test/attendee_test.js
+++ test/attendee_test.js
@@ -18,7 +18,7 @@ describe('AttendeePrez', function() {
 
     it('should show a question on page', function() {
       browser.assert.hasClass('#page_1','visible');
-      browser.assert.text('#page_1', "Qu'avez penser de cette conférence ?");
+      browser.assert.text('#page_1', "Qu'avez vous pensé de cette conférence ?");
     });
     
     it('should show the answer panel', function() {
